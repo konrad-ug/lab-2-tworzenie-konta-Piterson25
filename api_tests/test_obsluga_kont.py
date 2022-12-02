bbc495903a602975cb15fa998df14363bd4d05d7
Piterson25 <piotrmaszczak25@gmail.com>
2022-12-02 17:59:23 +0100

    Feature 15

--- api_tests/test_obsluga_kont.py
+++ api_tests/test_obsluga_kont.py
@@ -3,15 +3,21 @@ import requests
 
 
 # odpalanie flaska
-# export FLASK_APP=api/api.py
+# export FLASK_APP=app/api.py
 # python3 -m flask run
 # odpalanie testu
-# python3 -m unittest api_test/tests_obsluga_kont.py
+# python3 -m unittest api_tests/test_obsluga_kont.py
 
 class TestObslugaKont(unittest.TestCase):
     body = {
         "imie": "nick",
         "nazwisko": "cave",
+        "pesel": "01292909876"
+    }
+
+    body_false = {
+        "imie": "mike",
+        "nazwisko": "baron",
         "pesel": "01292909876"
     }
 
@@ -26,21 +32,26 @@ class TestObslugaKont(unittest.TestCase):
         create_resp = requests.post(self.url + "/konta/stworz_konto", json=self.body)
         self.assertEqual(create_resp.status_code, 201)
 
-    def test_2_get_po_peselu(self):
+    def test_2_tworzenie_istniejacego_konta(self):
+        create_resp = requests.post(self.url + "/konta/stworz_konto", json=self.body_false)
+        self.assertEqual(create_resp.status_code, 400)
+
+    def test_3_get_po_peselu(self):
         get_resp = requests.get(self.url + f"/konta/konto/{self.body['pesel']}")
         self.assertEqual(get_resp.status_code, 200)
         resp_body = get_resp.json()
-        self.assertEqual(resp_body["nazwisko"], self.body["nazwisko"])
         self.assertEqual(resp_body["imie"], self.body["imie"])
+        self.assertEqual(resp_body["nazwisko"], self.body["nazwisko"])
         self.assertEqual(resp_body["saldo"], 0)
 
-    def test_3_put_po_peselu(self):
-        put_resp = requests.put(self.url + f"/konta/update/{self.body['pesel']}", json=self.body2)
+    def test_4_put_po_peselu(self):
+        put_resp = requests.put(self.url + f"/konta/konto/{self.body['pesel']}", json=self.body2)
         self.assertEqual(put_resp.status_code, 200)
         resp_body = put_resp.json()
-        self.assertEqual(resp_body["nazwisko"], self.body2["nazwisko"])
-        self.assertEqual(resp_body["imie"], self.body2["imie"])
+        self.assertEqual(resp_body["imie"], "bob")
+        self.assertEqual(resp_body["nazwisko"], "marley")
+        self.assertEqual(resp_body["pesel"], "01292909876")
 
-    def test_4_delete_po_peselu(self):
+    def test_5_delete_po_peselu(self):
         delete_resp = requests.delete(self.url + f"/konta/delete/{self.body['pesel']}")
         self.assertEqual(delete_resp.status_code, 201)
--- app/api.py
+++ app/api.py
@@ -9,22 +9,12 @@ app = Flask(__name__)
 def stworz_konto():
     dane = request.get_json()
     print(f"Request o stworzenie konta z danymi: {dane}")
-    konto = Konto(dane["imie"], dane["nazwisko"], dane["pesel"])
-    RejestrKont.add_account(konto)
-    return jsonify("Konto stworzone"), 201
-
-
-@app.route("/konta/update/<pesel>", methods=['PUT'])
-def aktualizuj_konto(pesel):
-    dane = request.get_json()
-    print(f"Request o update konta z danymi: {dane}")
-    konto = RejestrKont.find_account(pesel)
-    print("konto znalezione")
-    konto.imie = dane["imie"] if "imie" in dane else konto.imie
-    konto.nazwisko = dane["nazwisko"] if "nazwisko" in dane else konto.nazwisko
-    konto.pesel = dane["pesel"] if "pesel" in dane else konto.pesel
-    konto.saldo = dane["saldo"] if "saldo" in dane else konto.saldo
-    return jsonify("Update zakonczony powodzeniem"), 200
+    if RejestrKont.find_account(dane["pesel"]):
+        return jsonify("Konto z podanym peselem juz istnieje"), 400
+    else:
+        konto = Konto(dane["imie"], dane["nazwisko"], dane["pesel"])
+        RejestrKont.add_account(konto)
+        return jsonify("Konto stworzone"), 201
 
 
 @app.route("/konta/ile_kont", methods=['GET'])
@@ -36,6 +26,19 @@ def ile_kont():
 def wyszukaj_konto_z_peselem(pesel):
     print(f"Request o konto z peselem: {pesel}")
     konto = RejestrKont.find_account(pesel)
+    return jsonify(imie=konto.imie, nazwisko=konto.nazwisko, pesel=konto.pesel, saldo=konto.saldo), 200
+
+
+@app.route("/konta/konto/<pesel>", methods=['PUT'])
+def aktualizuj_konto(pesel):
+    dane = request.get_json()
+    print(f"Request o update konta z danymi: {dane}")
+    konto = RejestrKont.find_account(pesel)
+    print("konto znalezione")
+    konto.imie = dane["imie"] if "imie" in dane else konto.imie
+    konto.nazwisko = dane["nazwisko"] if "nazwisko" in dane else konto.nazwisko
+    konto.pesel = dane["pesel"] if "pesel" in dane else konto.pesel
+    konto.saldo = dane["saldo"] if "saldo" in dane else konto.saldo
     return jsonify(imie=konto.imie, nazwisko=konto.nazwisko, pesel=konto.pesel, saldo=konto.saldo), 200
 
 
