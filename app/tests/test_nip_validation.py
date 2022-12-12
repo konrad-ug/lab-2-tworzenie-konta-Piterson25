281af947b040717fb72e4aa3b69c516665822e14
Piterson25 <piotrmaszczak25@gmail.com>
2022-12-12 15:31:02 +0100

    Nip fix

--- app/tests/test_nip_validation.py
+++ app/tests/test_nip_validation.py
@@ -4,11 +4,11 @@ from ..KontoFirmowe import KontoFirmowe
 
 class TestNipValidation(unittest.TestCase):
     @patch('app.KontoFirmowe.KontoFirmowe.czy_nip_istnieje', return_value=True)
-    def test_1_prawidlowy_nip(self):
-        konto_firmowe = KontoFirmowe("Jakub", "42142149417")
-        self.assertEqual(konto_firmowe.nip, "42142149417")
+    def test_1_prawidlowy_nip(self, mock_konto_firmowe):
+        konto_firmowe = KontoFirmowe("Jakub", "4214214941")
+        self.assertEqual(konto_firmowe.nip, "4214214941")
 
     @patch('app.KontoFirmowe.KontoFirmowe.czy_nip_istnieje', return_value=None)
-    def test_2_nieprawidlowy_nip(self):
+    def test_2_nieprawidlowy_nip(self, mock_konto_firmowe):
         konto_firmowe = KontoFirmowe("Arek", "0000000000")
         self.assertEqual(konto_firmowe.nip, "Pranie!")
